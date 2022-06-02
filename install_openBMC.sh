62cb243dc82ed8ca82ad8adb61174faa280c0540
littlebee21 <805976520@qq.com>
2022-06-02 11:52:36 +0800

    open

---
+++ install_openBMC.sh
@@ -0,0 +1,8 @@
+#安装支持软件
+sudo apt-get install -y git build-essential libsdl1.2-dev texinfo gawk chrpath diffstat
+
+sudo apt-get install -y gawk wget git-core git diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev pylint3 xterm sed cvs subversion help2man make gcc g++ desktop-file-utils libgl1-mesa-dev libglu1-mesa-dev mercurial autoconf automake groff curl lzop asciidoc npm meson ninja tree
+
+
+#克隆
+git clone git@github.com:openbmc/openbmc.git
---
+++ run_openBMCi_by_port.sh
@@ -0,0 +1,8 @@
+sudo qemu-system-arm -m 256 \
+	-M romulus-bmc -nographic -drive \
+	file=./tmp/deploy/images/romulus/obmc-phosphor-image-romulus.static.mtd,format=raw,\
+	if=mtd -net nic -net user,\
+	hostfwd=:127.0.0.1:2222-:22,\
+	hostfwd=:127.0.0.1:2443-:443,\
+	hostname=qemu
+
